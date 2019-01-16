1a17511123adc9db38f66c644007063d78b3c461
godblessjoel <44238619+godblessjoel@users.noreply.github.com>
2019-01-16 12:43:13 -0500

    first commit

---
+++ pokemon.py
@@ -0,0 +1,30 @@
+import time
+
+print('_' * 60)
+print('A wild Vulpix appears!')
+time.sleep(0.2)
+print('You only have one Pokemon, Vulpix.')
+time.sleep(0.2)
+print('I choose you Vulpix!')
+print()
+time.sleep(0.2)
+
+vulpix_hp = 200
+vulpix_hp = 125
+
+print('Battle Options:')
+time.sleep(0.2)
+print('- [1] Flamethrower')
+time.sleep(0.2)
+print('- [2] Ember')
+time.sleep(0.2)
+print('-[3] fire fang')
+time.sleep(0.2)
+print('-[4] Tackle')
+time.sleep(0.2)
+your_move = input('Choose a move using the corresponding number: ')
+if your_move == '1'
+	print('Vulpix uses Fire fang ')
+	vulpix_hp = vulpix_hp + 50
+	print('vulpix uses Fire fang, his HP increases to ' + str(snorlax_hp))
+	time.sleep(0.2)
